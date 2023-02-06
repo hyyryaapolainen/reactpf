bc43c7dafde6cb1c5c85de18e1387559b31516b5
hyyryaapolainen <a.hyyrylainen79@gmail.com>
2023-02-06 16:07:15 +0200

    Updated portfolio

---
+++ build/precache-manifest.68cc757c27d6cd2ad66e95379550ca54.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e300c38087129a084143b933ce18f1",
+    "url": "/reactpf/index.html"
+  },
+  {
+    "revision": "d31a824dc51cab51ed90",
+    "url": "/reactpf/static/css/2.246d7c7d.chunk.css"
+  },
+  {
+    "revision": "0a29f7009b280c43e5a6",
+    "url": "/reactpf/static/css/main.175f335d.chunk.css"
+  },
+  {
+    "revision": "d31a824dc51cab51ed90",
+    "url": "/reactpf/static/js/2.fd87cda8.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/reactpf/static/js/2.fd87cda8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a29f7009b280c43e5a6",
+    "url": "/reactpf/static/js/main.453079d5.chunk.js"
+  },
+  {
+    "revision": "613761695ec1df8fe0f7",
+    "url": "/reactpf/static/js/runtime-main.3c522a1e.js"
+  },
+  {
+    "revision": "aa2ff6821f8002aaf882f0ceaf1ace78",
+    "url": "/reactpf/static/media/Harava.aa2ff682.gif"
+  },
+  {
+    "revision": "4782c48c11be527f3e154ece9fa73161",
+    "url": "/reactpf/static/media/Kuva1.4782c48c.png"
+  },
+  {
+    "revision": "7708ed5d8df3ebbca542d9da82519de5",
+    "url": "/reactpf/static/media/RUIN.7708ed5d.png"
+  },
+  {
+    "revision": "4edeb595feca3490e3dbc8184bbfa1ae",
+    "url": "/reactpf/static/media/dump (2).4edeb595.png"
+  },
+  {
+    "revision": "4d590da6e62788505e7c341d26b2b48e",
+    "url": "/reactpf/static/media/outdoor (25) edited.4d590da6.png"
+  },
+  {
+    "revision": "750f66201f35b2eba70076d65d5475b7",
+    "url": "/reactpf/static/media/outdoor (41).750f6620.JPG"
+  }
+]);
